afaf197ada3ce423bf71a14de151641d8559f04a
dinahh <dinah.hunde@ten10.com>
2020-06-18 15:19:15 +0100

    clear filter test

--- src/test/java/pageObjects/ProductPage.java
+++ src/test/java/pageObjects/ProductPage.java
@@ -72,6 +72,24 @@ public class ProductPage extends BasePage {
 
     public void clearFilter(){ waitAndClick(CLEAR_FILTERS); }
 
+    public void sevenProducts(String input){
+        driver.getPageSource().contains(input);
+    }
+
+
+    public void clearFilter(){ waitAndClick(CLEAR_FILTERS); }
+
+//    public void sevenProducts() {
+//        WebElement sevenProducts = driver.findElement(By.xpath("//*[text()='There are 7 products.']"));
+//        Assert.assertTrue(elementIsVisible(sevenProducts));
+//    }
+    public void sevenProducts(String input){
+        driver.getPageSource().contains(input);
+    }
+
+
+    public void clearFilter(){ waitAndClick(CLEAR_FILTERS); }
+
 //    public void sevenProducts() {
 //        WebElement sevenProducts = driver.findElement(By.xpath("//*[text()='There are 7 products.']"));
 //        Assert.assertTrue(elementIsVisible(sevenProducts));
