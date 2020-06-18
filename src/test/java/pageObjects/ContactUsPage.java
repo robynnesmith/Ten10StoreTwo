458383956fd15a272a96c85fc783a65978a088e8
Zak <zakwannoor@gmail.com>
2020-06-18 15:24:31 +0100

    BuyJourneyZak - clean up code

--- src/test/java/pageObjects/ContactUsPage.java
+++ src/test/java/pageObjects/ContactUsPage.java
@@ -15,31 +15,35 @@ public class ContactUsPage extends BasePage {
     }
 
     WebDriverWait wait = new WebDriverWait(driver, 5);
+    private static final By CONTACT_US_PAGE = By.cssSelector("a[href='http://3.11.70.191/index.php?controller=contact']");
+    private static final By EMAIL = By.cssSelector("div[class='col-md-6'] >input.form-control");
+    private static final By MESSAGE = By.cssSelector("div[class='col-md-9'] >textarea");
+    private static final By SEND = By.cssSelector("input[class='btn btn-primary']");
+    private static final By SENT_MESSAGE = By.cssSelector("div[class='col-xs-12 alert alert-success']");
 
 
     public void goToContactUsPage() {
-        WebElement contactUsPage = driver.findElement(By.cssSelector("a[href='http://3.11.70.191/index.php?controller=contact']"));
+        WebElement contactUsPage = driver.findElement(CONTACT_US_PAGE);
         contactUsPage.click();
     }
 
     public void enterEmailAddress() {
-        WebElement email = driver.findElement(By.cssSelector("div[class='col-md-6'] >input.form-control"));
+        WebElement email = driver.findElement(EMAIL);
         email.sendKeys("zaknoor@fakemail.com");
     }
 
     public void enterMessage() {
-        WebElement message = driver.findElement(By.cssSelector("div[class='col-md-9'] >textarea"));
+        WebElement message = driver.findElement(MESSAGE);
         message.sendKeys("skdfjlfkdjs");
     }
 
     public void clickSend() {
-        WebElement send = driver.findElement(By.cssSelector("input[class='btn btn-primary']"));
+        WebElement send = driver.findElement(SEND);
         send.click();
     }
 
-
     public void verifySentMessage() {
-        WebElement sentMessage = driver.findElement(By.cssSelector("div[class='col-xs-12 alert alert-success']"));
+        WebElement sentMessage = driver.findElement(SENT_MESSAGE);
         wait.until(ExpectedConditions.visibilityOf(sentMessage));
         System.out.println(sentMessage.getText());
         Assert.assertTrue(sentMessage.isDisplayed());
