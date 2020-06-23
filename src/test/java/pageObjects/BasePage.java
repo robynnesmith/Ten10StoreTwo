d18d159d86cf0a1215ec1dd5dcba29105fb2dcd1
robynne.smith <robynne.smith@ten10.com>
2020-06-23 08:30:03 +0100

    updated driver location and added cucumber runner

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static Tests.Registration.driver;
-
 /**
  * Created by jack.forman on 16/10/2016.
  */
